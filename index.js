8c8270e276af0d3d8d523ae1b88337eba792b793
“regmibishal42” <regmibishal42@gmail.com>
2022-12-21 16:28:36 +0545

    Setting Up App

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-import express from '@express';
-// hello
+import express from 'express';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+
+
+
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server Running on port ${process.env.PORT}`);
+});
